14499467c11a4df44b3b3bfdf8c40af16930da1d
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-02 21:43:02 -0300

    fix: Error en el comando sed

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,4 +1,4 @@
   #!/bin/bash
   cd /home/ubuntu/esi-pruebas/deploy
-  sed -i "esi-pruebas:.*/esi-pruebas:$1/g" docker-compose.yml
+  sed -i "esi-pruebas:.*esi-pruebas:$1/g" docker-compose.yml
   docker-compose up -d
