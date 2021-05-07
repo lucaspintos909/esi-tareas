2668e53f131ad987f37e54e0e00e26a59dc240a1
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-07 01:25:21 -0300

    feat: Navbar con bootstrap

--- src/includes/header.php
+++ src/includes/header.php
@@ -15,3 +15,31 @@
 </head>
 
 <body>
+
+
+<nav class="navbar navbar-dark navbar-expand-lg bg-primary shadow" >
+    <div class="container">
+        <a class="navbar-brand" href="<?=URL?>">Pruebas Esi</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?=URL?>">Inicio</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Practicos
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="<?=URL?>practico1">
+                            Practico 1
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
