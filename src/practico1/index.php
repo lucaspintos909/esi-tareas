c5697dfe72a137adf481d95e615c44795aae5979
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-08 13:08:50 -0300

    refactor: Añadiendo ejercicio 4 al listado

--- src/practico1/index.php
+++ src/practico1/index.php
@@ -11,7 +11,8 @@ include_once '../includes/header.php';
     <div class="list-group mt-3 w-50 shadow-sm">
         <a href="<?=URL?>practico1/ejercicio1-calculadora.php" class="list-group-item list-group-item-action">Ejericio 1 - Calculadora</a>
         <a href="<?=URL?>practico1/ejercicio2-notas.php" class="list-group-item list-group-item-action">Ejericio 2 - Notas</a>
-        <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+        <a href="<?=URL?>practico1/ejercicio3-concatenando.php" class="list-group-item list-group-item-action">Ejercicio 3 - Concatenando</a>
+        <a href="<?=URL?>practico1/ejercicio4-recorriendo-arrays.php" class="list-group-item list-group-item-action">Ejercicio 4 - Recorriendo Arrays</a>
         <a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
     </div>
 </div>
