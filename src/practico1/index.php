135c3396ea93549f51e034c6876aaf5102cebad5
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-08 15:55:57 -0300

    refactor: Añadiendo titulo, y ejercicio 5

--- src/practico1/index.php
+++ src/practico1/index.php
@@ -7,12 +7,14 @@ include_once '../includes/header.php';
 ?>
 
 <div class="container mt-3">
-    <h1>Ejercicios</h1>
+    <h1>Practico 1</h1>
+    <h3 class="mt-5">Ejercicios</h3>
     <div class="list-group mt-3 w-50 shadow-sm">
-        <a href="<?=URL?>practico1/ejercicio1-calculadora.php" class="list-group-item list-group-item-action">Ejericio 1 - Calculadora</a>
-        <a href="<?=URL?>practico1/ejercicio2-notas.php" class="list-group-item list-group-item-action">Ejericio 2 - Notas</a>
+        <a href="<?=URL?>practico1/ejercicio1-calculadora.php" class="list-group-item list-group-item-action">Ejercicio 1 - Calculadora</a>
+        <a href="<?=URL?>practico1/ejercicio2-notas.php" class="list-group-item list-group-item-action">Ejercicio 2 - Notas</a>
         <a href="<?=URL?>practico1/ejercicio3-concatenando.php" class="list-group-item list-group-item-action">Ejercicio 3 - Concatenando</a>
         <a href="<?=URL?>practico1/ejercicio4-recorriendo-arrays.php" class="list-group-item list-group-item-action">Ejercicio 4 - Recorriendo Arrays</a>
+        <a href="<?=URL?>practico1/ejercicio5-array-multidimensional.php" class="list-group-item list-group-item-action">Ejercicio 5 - Recorriendo arrays multidimensionales</a>
         <a href="#" class="list-group-item list-group-item-action disabled">Vestibulum at eros</a>
     </div>
 </div>
