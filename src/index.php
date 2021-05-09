7b995b7ec788205627f17a4541d8605b54f0e92f
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-09 13:18:39 -0300

    refactor: Contenido random en index

--- src/index.php
+++ src/index.php
@@ -4,7 +4,21 @@ include_once './const.php';
 include_once 'includes/header.php';
 ?>
 
-
+<div class="container mt-5">
+    <div class="card text-center">
+    <div class="card-header d-flex">
+        Fecha : 8/5/2021
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">Práctico 1 - Programación Web</h5>
+        <p class="card-text">Variables, estructuras y formularios.<br>
+            Ejercicios del 1 al 6 terminados.<br>
+            <a href="https://pias.uy/publicpias/category/programacionweb/practicos-prog-web/">Link del practico</a>
+        </p>
+        <a href="<?=URL?>practico1" class="btn btn-primary">Ver ejercicios</a>
+    </div>
+    </div>
+</div>
 
 <?php
 include_once 'includes/footer.php';
