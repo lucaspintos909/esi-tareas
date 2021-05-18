0a023a8a9d28da38847f29984d30467b08695ae9
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-18 13:31:59 -0300

    refactor: Array con ejercicios/rutas

--- src/practico1/index.php
+++ src/practico1/index.php
@@ -4,18 +4,33 @@
 include_once '../const.php';
 
 include_once '../includes/header.php';
+
+$ejercicios = array(
+
+    "Ejercicio 1 - Calculadora"                             => "practico1/ejercicio1-calculadora.php",
+    "Ejercicio 2 - Notas"                                   => "practico1/ejercicio2-notas.php",
+    "Ejercicio 3 - Concatenando"                            => "practico1/ejercicio3-concatenando.php",
+    "Ejercicio 4 - Recorriendo Arrays"                      => "practico1/ejercicio4-recorriendo-arrays.php",
+    "Ejercicio 5 - Recorriendo arrays multidimensionales"   => "practico1/ejercicio5-array-multidimensional.php",
+    "Ejercicio 6 - Formularios"                             => "practico1/ejercicio6-formularios.php",
+
+);
+
 ?>
+
+
 
 <div class="container mt-3">
     <h1>Practico 1</h1>
     <h3 class="mt-5">Ejercicios</h3>
     <div class="list-group mt-3 w-50 shadow-sm">
-        <a href="<?=URL?>practico1/ejercicio1-calculadora.php" class="list-group-item list-group-item-action">Ejercicio 1 - Calculadora</a>
-        <a href="<?=URL?>practico1/ejercicio2-notas.php" class="list-group-item list-group-item-action">Ejercicio 2 - Notas</a>
-        <a href="<?=URL?>practico1/ejercicio3-concatenando.php" class="list-group-item list-group-item-action">Ejercicio 3 - Concatenando</a>
-        <a href="<?=URL?>practico1/ejercicio4-recorriendo-arrays.php" class="list-group-item list-group-item-action">Ejercicio 4 - Recorriendo Arrays</a>
-        <a href="<?=URL?>practico1/ejercicio5-array-multidimensional.php" class="list-group-item list-group-item-action">Ejercicio 5 - Recorriendo arrays multidimensionales</a>
-        <a href="<?=URL?>practico1/ejercicio6-formularios.php" class="list-group-item list-group-item-action">Ejercicio 6 - Formularios</a>
+
+        <?php
+        foreach ($ejercicios as $ejercicio => $ruta) {
+            echo '<a href="' . URL . $ruta . '" class="list-group-item list-group-item-action">' . $ejercicio . '</a>';
+        }
+        ?>
+        
     </div>
 </div>
 
