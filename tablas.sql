30f9d9ef2e6eec978686a6f660f809a94da52d44
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-19 00:35:07 -0300

    feat: Archivo para la inicializacion de tablas

---
+++ tablas.sql
@@ -0,0 +1,9 @@
+use tareas_esi;
+
+create table if not exists contacts(
+    phone varchar(9) primary key,
+    name varchar(15) not null ,
+    last_name varchar(15) not null
+);
+
+insert into contacts values ('21341234','Lucas','Pintos'), ('098123456','Agustin','Fernandez');
