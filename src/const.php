261ac5384e4b07d4a312dc0623fef8865e8f3beb
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-19 00:35:27 -0300

    feat: Constante para ejercicios

--- src/const.php
+++ src/const.php
@@ -2,6 +2,7 @@
 
 const URL = "http://localhost:8080/";
 const URL_PRACTICO1 = "http://localhost:8080/practico1/";
+const URL_EJERCICIOS = "http://localhost:8080/ejercicios/";
 
 const HOST = "db";
 const USER = "root";
